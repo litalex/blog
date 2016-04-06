bb6dadc4bd3d60b34a1290ef7b03c8a1a92728b7
alexandrl <alexandrl@migfin.com>
2016-04-06 14:22:52 +0300

    Install and configure app

--- src/Litalex/AppBundle/AppBundle.php
+++ src/Litalex/AppBundle/AppBundle.php
@@ -1,9 +1,14 @@
 <?php
 
-namespace AppBundle;
+namespace Litalex\AppBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class AppBundle
+ *
+ * @package Litalex\AppBundle
+ */
 class AppBundle extends Bundle
 {
 }
